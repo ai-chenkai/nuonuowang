30e1b57332f8bef643fc5a6f4e8d539bad76cdd5
chenkai <chenkai@qq.com>
2021-01-15 10:31:00 +0800

    同时重写HashCode（）和equals()方法，判断商品是否是同一个商品

--- maven_test/src/main/java/com/chenkai/homework/selectShangPin.java
+++ maven_test/src/main/java/com/chenkai/homework/selectShangPin.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class selectShangPin {
     public static void main(String[] args) {
-        shangpin s1=new shangpin();
-        shangpin s2=new shangpin();
+        shangpin s1=new shangpin("平板",1);
+        shangpin s2=new shangpin("平板",1);
         System.out.println(s1==s2);
         Map m=new HashMap();
         m.put(s1,1);
